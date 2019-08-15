c4cab0c05e1eb8e79df86981b7e5e38e43d398dd
Wilson Valente <valentephd@gmail.com>
2019-08-15 17:53:26 -0300

    Cadastrar e Listar

--- RAIuri/index.php
+++ RAIuri/index.php
@@ -1,6 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema de Cadastro de Veículos - SCV</title>
+</head>
+<body>
+    <h1>Sistema de Cadastro de Veículos - SCV</h1>
+    <h2>Menu</h2>
+    <br>
+    <ul>
+        <li><a href="views/cadastrar.php">CADASTRAR UM VEÍCULO</a></li>
+        <br>
+        <li><a href="views/listar.php">LISTAR TODOS OS VEÍCULOS</a></li>
+    </ul>
+    <br>
+    <br>
+    <hr>
+    <footer>
+        <h3>Iuri Soares</h3>
+        <h4>RA: 1234567</h4>
+    </footer>
+</body>
+</html>
+
+
 <?php
 
-echo "Tela Inicial";
-
+//
 
 ?>
