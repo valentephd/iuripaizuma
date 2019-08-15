12559ff8e9a8d6ad9d4594827d04435d0424f1ed
Wilson Valente <valentephd@gmail.com>
2019-08-15 20:07:42 -0300

    Create controle and Form Cadastrar basic

---
+++ RAIuri/controllers/cadastroController.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+// TODO
+// Instanciar Classe correta (de acordo com o tipo do veiculo)
+// Chamar o método insert da classe para o obj
+
+// Obs/Dúvida ... A classe já vai incluir o veículo e depois incluir o específico (carro/ônibus/caminhão)
--- RAIuri/views/cadastrar.php
+++ RAIuri/views/cadastrar.php
@@ -8,8 +8,54 @@
 </head>
 <body>
     <h1>SVC - Cadastro de Veículo</h1>
-    <form action="">
-        <!--    //TODO  -->
+    <form action="../controllers/cadastroController.php" method="post">
+        <span>Tipo: </span>
+        <select name="tipo">
+            <option value="">Selecione um Tipo de Veículo</option>
+            <option value="1">Carro</option>
+            <option value="2">Ônibus</option>
+            <option value="3">Caminhão</option>
+        </select>
+        <br><br>
+        <span>Placa: </span>
+        <input type="text" name="placa">
+        <br><br>
+        <span>Nº Chassi: </span>
+        <input type="text" name="numChassi">
+        <br><br>
+        <span>Cor: </span>
+        <input type="text" name="cor">
+        <br><br>
+        <span>Ano: </span>
+        <input type="text" name="ano">
+        <br><br>
+        <span>Marca: </span>
+        <input type="text" name="marca">
+        <br><br>
+        <span>Modelo</span>
+        <input type="text" name="modelo">
+        <br><br>
+        <span>Peso Máximo: </span>
+        <input type="text" name="pesoMaximo">
+        <br><br>
+        <span>Preço: </span>
+        <input type="text" name="preco">
+        <br><br>
+        <span>Nº Rodas: </span>
+        <input type="text" name="numRodas">
+        <!-- Específicos -->
+        <br><br>
+        <span>Quantidade de Passageiros: </span>
+        <input type="text" name="qtdPassageiros">
+        <br><br>
+        <span>Nº Portas: </span>
+        <input type="text" name="numPortas">
+        <br><br>
+        <span>Quantidade de Eixos: </span>
+        <input type="text" name="quantidadeEixo">
+        <br>
+        <br>
+        <input type="submit" value="Cadastrar">
     </form>
 </body>
 </html>
